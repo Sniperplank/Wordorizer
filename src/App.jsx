187e47dfdd27b940ce91bf0b0b4a8bc67ab2d6aa
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-19 11:51:26 -0500

    dependensies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Stack } from '@mui/material'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/memorized' element={<Login />} />
+        <Route exact path='/quiz' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
