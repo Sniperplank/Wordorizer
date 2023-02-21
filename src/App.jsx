e655c10d1f10b431b0a04a2d3355ba30929d0931
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-20 23:27:47 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
 import './App.css'
 import Home from './Pages/Home';
 import MemorizedWords from './Pages/MemorizedWords';
 import Quiz from './Pages/Quiz';
+import { NavButton } from './StyledComponents/NavButton';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/memorized' element={<MemorizedWords />} />
-      <Route path='/quiz' element={<Quiz />} />
-    </Routes>
+    <Stack>
+      <Stack direction='row' spacing={5} justifyContent='center'>
+        <NavButton component={Link} to='/' variant='text'>All Words</NavButton>
+        <NavButton component={Link} to='/memorized' variant='text'>Review</NavButton>
+        <NavButton component={Link} to='/quiz' variant='text'>Quiz Me</NavButton>
+      </Stack>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/memorized' element={<MemorizedWords />} />
+        <Route path='/quiz' element={<Quiz />} />
+      </Routes>
+    </Stack>
   )
 }
 
