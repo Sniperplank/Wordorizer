2cb902e72679e18365b009379a940f62aa09a083
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-24 00:51:36 -0500

    name display

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuth } from '../contexts/AuthContext'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const { user, setUser } = useAuth()
+
+    return (
+        <div>{user?.result.name}</div>
+    )
 }
 
 export default Home
