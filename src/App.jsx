4f41beb72c187674b246e317d90f6d4234e97eaa
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-24 00:51:30 -0500

    user

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,27 @@ import MemorizedWords from './Pages/MemorizedWords';
 import Quiz from './Pages/Quiz';
 import { NavButton } from './StyledComponents/NavButton';
 import Signin from './Pages/Signin';
+import { useAuth } from './contexts/AuthContext'
 
 function App() {
+  const { user, setUser } = useAuth()
+
+  const logout = () => {
+    localStorage.clear()
+    setUser(null)
+    navigate('/')
+  }
+
   return (
     <Stack>
       <Stack direction='row' spacing={5} justifyContent='space-evenly' p={4}>
         <NavButton component={Link} to='/' variant='text'>All Words</NavButton>
         <NavButton component={Link} to='/memorized' variant='text'>Review</NavButton>
         <NavButton component={Link} to='/quiz' variant='text'>Quiz Me</NavButton>
+        {
+          user ? <NavButton onClick={logout} variant='text'>Sign Out</NavButton> :
+            <NavButton component={Link} to='/signin' variant='text'>Sign in</NavButton>
+        }
       </Stack>
       <Routes>
         <Route path='/' element={<Home />} />
