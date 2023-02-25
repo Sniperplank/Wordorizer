358d34f5b9794f2408ca570c81dd32703adfd718
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-25 00:30:32 -0500

    modal components

---
+++ src/StyledComponents/ModalContent.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const ModalContent = styled(Box)(({ theme }) => ({
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: theme.palette.secondary.main,
+    padding: '50px',
+    zIndex: 1000,
+    borderRadius: 10,
+    color: theme.palette.text.main,
+}));
