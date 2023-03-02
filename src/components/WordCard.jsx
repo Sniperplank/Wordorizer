7b6dc630a857d92450e8bfd8cbe201b2777ff4da
Eilaf Aljundi <elafjundi@gmail.com>
2023-03-01 23:23:18 -0500

    wordcard bug++

--- src/components/WordCard.jsx
+++ src/components/WordCard.jsx
@@ -24,9 +24,9 @@ function WordCard({ word, onClick, isClicked, update }) {
     return (
         <CardBox onClick={onClick} sx={{ justifyContent: 'center', alignItems: 'center' }}>
             <Stack direction='row' spacing={5}>
-                <Typography variant='h5' color='primary'>{word.word}</Typography>
+                <Typography variant='h5' color='primary' onClick={(event) => { event.stopPropagation() }}>{word.word}</Typography>
                 <Typography variant='h5'>:</Typography>
-                <Typography variant='h5'>{word.definition}</Typography>
+                <Typography variant='h5' onClick={(event) => { event.stopPropagation() }}>{word.definition}</Typography>
                 {
                     isClicked &&
                     <Stack direction='row' spacing={2}>
